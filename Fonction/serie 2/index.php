5abd12dd47b90a601d7fb4a59c3eea7eedf9223e
Laureenb <laureen_bataille@hotmail.com>
2017-11-17 16:29:02 +0100

    fonction serie 2

--- Fonction/serie 2/index.php
+++ Fonction/serie 2/index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<title></title>
+</head>
+<body>
+
 <?php
 
 // Crée une fonction te permettant de gérer des messages envers l'utilisateur grâce à deux arguments: le premier argument est le message, le second permet de spécifier un attribut de classe html qui sera utilisée par le CSS (par exemple: "info", "error", "warning"). Grâce à cette fonction, on pourra écrire en php:
@@ -5,10 +13,21 @@
 // ce qui provoquera cette injection d'html:
 // <div class="warning">Adresse email incorrecte.</div>
 
+// On crée la fonction 
 
+function afficher_classe($classe, $message){
+	echo ('<div class ="'.$classe.'">'.$message.'</div>');
+}
+
+// On déclare les variables des différents types de messages
+
+afficher_classe('info', 'Ceci est un message d\'info');
+afficher_classe('alert', 'Ceci est un message alerte');
+afficher_classe('error', 'Ceci est un message d\'erreur'); 
     
-
-
 // Trouve par toi-même dans la documentation php comment modifier ta fonction pour que, si le second argument n'est pas spécifié, sa valeur soit égale à "info".
 
 ?>
+
+</body>
+</html>
